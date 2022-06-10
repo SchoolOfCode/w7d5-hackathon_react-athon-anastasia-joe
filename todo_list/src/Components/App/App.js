324de44d6c4fc5d93924670ba40332e9830103a5
JojokCreator <joeyk2k@hotmail.com>
2022-06-10 13:04:58 +0100

    progress 7a

--- todo_list/src/Components/App/App.js
+++ todo_list/src/Components/App/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import Input from '../Input'
 import List from '../List'
 import ListItem from '../List/ListItem'
-
+import { useState } from 'react'
 
 
 function App() {
+    const [toDo, setToDo] = useState('')
 
-  return (
+    function handleChange(e) {
+      console.log(e.target.value);
+      setToDo(e.target.value) 
+
+    }
+  
+    function onClickToDo () {
+      console.log(toDo)
+      //addTask(task) function
+    }
+
+    return (
     <div className="App">
-    <Input />
+    <Input theValue={handleChange} onClickToDo={onClickToDo} />
     <List >
       <ListItem />
       <ListItem />
--- todo_list/src/Components/Input/index.js
+++ todo_list/src/Components/Input/index.js
@@ -1,31 +1,12 @@
 import React, { useState } from 'react'
 
-
 const Input = (props) => {
-
-    const [toDo, setToDo] = useState('')
-
-  function handleChange(e) {
-    console.log(e.target.value);
-    setToDo(e.target.value) 
-    console.log(setToDo)
-  }
-
-  function onClickToDo (e) {
-    // console.log(e.target.value)
-    console.log(e.target.value);
-    setToDo(e.target.value) 
-    console.log(setToDo)
-  }
-
-
-
 
        return (
         <div>
             <h1>Todo List</h1>
             <input onChange={props.theValue} ></input>
-            <button onClick >Add a todo</button>
+            <button onClick={props.onClickToDo} >Add a todo</button>
             <br/>
             <h3>Filters:</h3>
             <button>Pending</button>
